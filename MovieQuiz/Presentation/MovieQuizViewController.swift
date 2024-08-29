34c78c309915c059457695abc6bdc2467ee138b9
NikZebolov <zebolovnik@yandex.ru>
2024-08-29 21:31:51 +1000

    Шрифт корректно отображается; закругление рамок; view models - privat

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -3,36 +3,24 @@ import UIKit
 final class MovieQuizViewController: UIViewController {
     
     // MARK: - Data Models (модели данных для состояний)
-    // для состояния "Вопрос показан"
-    struct QuizStepViewModel {
-        // картинка с афишей фильма с типом UIImage
+    private struct QuizStepViewModel {
         let image: UIImage
-        // вопрос о рейтинге квиза
         let question: String
-        // строка с порядковым номером этого вопроса (ex. "1/10")
         let questionNumber: String
     }
-
-    // для состояния "Результат квиза"
-    struct QuizResultsViewModel {
-        // строка с заголовком алерта
+    
+    private struct QuizResultsViewModel {
         let title: String
-        // строка с текстом о количестве набранных очков
         let text: String
-        // текст для кнопки алерта
         let buttonText: String
     }
-
-    // Для представления данных одного вопроса в викторине
-    struct QuizQuestion {
-        // cтрока с названием фильма, совпадает с названием картинки афиши фильма в Assets
+    
+    private struct QuizQuestion {
         let image: String
-        // строка с вопросом о рейтинге фильма
         let text: String
-        // правильный ли ответ на вопрос
         let correctAnswer: Bool
-        }
-
+    }
+    
     // MARK: - Outlets
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
@@ -42,11 +30,11 @@ final class MovieQuizViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Берём текущий вопрос из массива вопросов по индексу текущего вопроса
-        let currentQuestion = questions[currentQuestionIndex]
-        // Конвертируем модель вопроса во вью модель
+        guard let currentQuestion = questions[safe: currentQuestionIndex] else {
+            print("Wow, easy there, our array isn't infinite, you know!")
+            return
+        }
         let viewModel = convert(model: currentQuestion)
-        // Отображаем вопрос на экране
         show(quiz: viewModel)
         
     }
@@ -68,13 +56,10 @@ final class MovieQuizViewController: UIViewController {
     
     // MARK: - Private Properties
     
-    // по этому индексу будем искать вопрос в массиве, где индекс первого элемента 0, а не 1
     private var currentQuestionIndex = 0
-
-    // переменная со счётчиком правильных ответов
+    
     private var correctAnswers = 0
     
-    // массив вопросов
     private let questions: [QuizQuestion] = [
         QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
         QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
@@ -90,38 +75,28 @@ final class MovieQuizViewController: UIViewController {
     
     // MARK: - Методы
     
-    // конвертация, - принимает моковый вопрос и возвращает вью модель для главного экрана
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
-            // Преобразуем строку с именем изображения в UIImage
-            image: UIImage(named: model.image) ?? UIImage(),
-            // Показываем текст вопроса
-            question: model.text,
-            // Отображаем текущий из оставшихся вопросов (формат: 1/10)
+            image: UIImage(named: model.image) ?? UIImage(),            question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)"
         )
         return questionStep
     }
     
-    // вывод на экран вопроса
-    private func show(quiz step: QuizStepViewModel) {
-        // Сброс рамки перед показом нового вопроса
-        imageView.layer.borderWidth = 0
+    private func show(quiz step: QuizStepViewModel) {        imageView.layer.borderWidth = 0
         imageView.layer.borderColor = UIColor.clear.cgColor
         
-        // Устанавливаем новые данные для нового вопроса
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
     
-    // меняем цвет рамки
     private func showAnswerResult(isCorrect: Bool) {
         if isCorrect {
             correctAnswers += 1
         }
         
-        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         
@@ -130,7 +105,6 @@ final class MovieQuizViewController: UIViewController {
         }
     }
     
-    // логика перехода в один из сценариев
     private func showNextQuestionOrResult() {
         if currentQuestionIndex == questions.count - 1 {
             let text = "Ваш результат: \(correctAnswers)/10"
@@ -143,14 +117,12 @@ final class MovieQuizViewController: UIViewController {
         } else {
             currentQuestionIndex += 1
             
-            // показываем следующий вопрос
             let nextQuestion = questions[currentQuestionIndex]
             let viewModel = convert(model: nextQuestion)
             show(quiz: viewModel)
         }
     }
     
-    // показываем результат раунда квиза
     private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
             title: result.title,
@@ -161,7 +133,6 @@ final class MovieQuizViewController: UIViewController {
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             
-            // заново показываем первый вопрос
             let firstQuestion = self.questions[self.currentQuestionIndex]
             let viewModel = self.convert(model: firstQuestion)
             self.show(quiz: viewModel)
@@ -173,69 +144,3 @@ final class MovieQuizViewController: UIViewController {
     }
     
 }
-
-
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-*/
