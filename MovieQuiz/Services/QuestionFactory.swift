1d7bd0b674f70ec78d4d765263430d33164ebc2f
NikZebolov <zebolovnik@yandex.ru>
2024-09-15 12:52:41 +1000

    class QuestionFactory таперь final

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class QuestionFactory: QuestionFactoryProtocol {
+final class QuestionFactory: QuestionFactoryProtocol {
     private let moviesLoader: MoviesLoading
     private weak var delegate: QuestionFactoryDelegate?
     private var movies: [MostPopularMovie] = []
